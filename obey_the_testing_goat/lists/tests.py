04c292c3734de776c5757abae79e4d865ec0be83
entri <nithinmohan@entri.me>
2021-10-16 23:01:06 +0530

    Add lists tests

---
+++ obey_the_testing_goat/lists/tests.py
@@ -0,0 +1,9 @@
+from django.template.loader import render_to_string
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        expected_html = render_to_string('home.html')
+        self.assertEqual(html, expected_html)
+        
